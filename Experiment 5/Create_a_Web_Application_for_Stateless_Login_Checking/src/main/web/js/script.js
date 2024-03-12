cb05cd82c078d965185cb338797f9b017732c198
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2024-03-12 14:29:59 +0530

    UPDATE: 12th March 2024

---
+++ Experiment 5/Create_a_Web_Application_for_Stateless_Login_Checking/src/main/web/js/script.js
@@ -0,0 +1,32 @@
+const users = [
+    { username: 'ABC', password: 'pass1' },
+    { username: 'DEF', password: 'pass2' },
+    { username: 'HIJ', password: 'pass3' },
+    { username: 'KLM', password: 'pass4' },
+    { username: 'OPQ', password: 'pass5' },
+];
+
+function userExists(username) {
+    return users.some(user => user.username === username);
+}
+
+function checkLogin() {
+    const usernameInput = document.getElementById('username').value;
+    const passwordInput = document.getElementById('password').value;
+    const loginMessage = document.getElementById('loginMessage');
+
+    if (userExists(usernameInput)) {
+        const user = users.find(user => user.username === usernameInput);
+
+        if (user.password === passwordInput) {
+            loginMessage.innerText = 'Login successful';
+            loginMessage.style.color = 'green';
+        } else {
+            loginMessage.innerText = 'Incorrect password';
+            loginMessage.style.color = 'red';
+        }
+    } else {
+        loginMessage.innerText = 'User does not exist';
+        loginMessage.style.color = 'red';
+    }
+}
---
+++ Experiment 5/Create_a_Web_Application_for_Stateless_Login_Checking/src/test/java/TestClass.java
@@ -0,0 +1,82 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class TestClass {
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        System.getProperty("webdriver.gecko.driver", "D:\\GitHub\\TA Lab\\geckodriver-v0.34.0-win-aarch64\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.get("D:\\GitHub\\TA Lab\\TA-Lab\\Experiment 5\\Create_a_Web_Application_for_Stateless_Login_Checking\\src\\main\\web\\index.html");
+    }
+
+    @Test
+    public void testLoginFailureUserNotFound() {
+        WebElement usernameInput = driver.findElement(By.id("username"));
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.tagName("button"));
+
+        usernameInput.sendKeys("nonexistentuser");
+        passwordInput.sendKeys("invalidpassword");
+        loginButton.click();
+
+        WebElement loginMessage = driver.findElement(By.id("loginMessage"));
+        Assert.assertEquals(loginMessage.getText(), "User does not exist");
+        Assert.assertEquals(loginMessage.getCssValue("color"), "rgb(255, 0, 0)"); // Red color
+    }
+
+    @Test
+    public void testLoginSuccess() {
+        WebElement usernameInput = driver.findElement(By.id("username"));
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.tagName("button"));
+
+        usernameInput.sendKeys("ABC");
+        passwordInput.sendKeys("pass1");
+        loginButton.click();
+
+        WebElement loginMessage = driver.findElement(By.id("loginMessage"));
+        Assert.assertEquals(loginMessage.getText(), "Login successful");
+        Assert.assertEquals(loginMessage.getCssValue("color"), "rgb(0, 128, 0)"); // Green color
+    }
+
+    @Test
+    public void testLoginFailureIncorrectPassword() {
+        WebElement usernameInput = driver.findElement(By.id("username"));
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.tagName("button"));
+
+        usernameInput.sendKeys("ABC");
+        passwordInput.sendKeys("pass11");
+        loginButton.click();
+
+        WebElement loginMessage = driver.findElement(By.id("loginMessage"));
+        Assert.assertEquals(loginMessage.getText(), "Incorrect password");
+        Assert.assertEquals(loginMessage.getCssValue("color"), "rgb(255, 0, 0)"); // Green color
+    }
+
+    @Test
+    public void testLoginSuccessAnotherUser() {
+        WebElement usernameInput = driver.findElement(By.id("username"));
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.tagName("button"));
+
+        usernameInput.sendKeys("DEF");
+        passwordInput.sendKeys("pass2");
+        loginButton.click();
+
+        WebElement loginMessage = driver.findElement(By.id("loginMessage"));
+        Assert.assertEquals(loginMessage.getText(), "Login successful");
+        Assert.assertEquals(loginMessage.getCssValue("color"), "rgb(0, 128, 0)"); // Green color
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
